675b41a58f4b8c04d6ff152a4adff92323b12da0
Vojtěch Michalec <michalecvojtech18@gmail.com>
2026-06-12 10:16:42 +0200

    Fix missing using for GetRequiredService extension method

--- App.axaml.cs
+++ App.axaml.cs
@@ -7,6 +7,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
 using OddiloveVybaveni.ViewModels;
 using OddiloveVybaveni.Views;
 
